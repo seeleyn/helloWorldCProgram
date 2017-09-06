488ae301d7c0018c665d2c8cc200d3801ff61eb4
Nate Seeley <seeleyn@adaptivecomputing.com>
2017-09-06 17:46:29 -0600

    most basic pointer examples

--- hello.c
+++ hello.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
 #include "functions.h"
 
+
+
+void basic() {
+  int radius=25;
+  int *radiusPtr = &radius;
+  int *otherRadiusPtr = radiusPtr;
+  printf("radiusPtr points to %d\n",*radiusPtr);
+  printf("otherRadiusPtr points to %d\n",*otherRadiusPtr);
+  printf("*&radius %d\n",*&radius);
+
+  printf("radiusPtr stores address      %u\n",radiusPtr);
+  printf("otherRadiusPtr stores address %u\n",otherRadiusPtr);
+}
+
 int main(void)
 {
-  int answer =  sum(2,2);
-  printf("The sum is %d\n",answer);
-  
+  printf("\n\n\n");
+  basic();
   return 0;
 }
