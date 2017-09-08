1a747118e117a1ee730f5b82e02c4d91d04f1a26
Nate Seeley <seeleyn@adaptivecomputing.com>
2017-09-07 18:06:18 -0600

    array pointer arithmetic example

--- hello.c
+++ hello.c
@@ -17,18 +17,28 @@ void basic() {
 
 
 void arrays() {
-  int arr[4] = {1, 2, 3, 4};
+  //basic array example
+  int arr[4] = {100, 200, 300, 400};
   printf("arr[0] is %d\n",arr[0]);
   printf("arr[1] is %d\n",arr[1]);
   printf("arr[2] is %d\n",arr[2]);
   printf("arr[3] is %d\n",arr[3]);
 
+  //example of using ++ to increment a pointer
   int *intPtr = arr;
   int i=0;
   for (i=0; i<4; i++) {
     printf("intPtr[%d] is %d\n",i,*intPtr);
     intPtr++;
   }
+
+  //example of pointer arithmetic
+  int *tmpPtr = arr;
+  printf("*(tmpPtr)   is %d\n",*(tmpPtr));
+  printf("*(tmpPtr+1) is %d\n",*(tmpPtr+1));
+  printf("*(tmpPtr+2) is %d\n",*(tmpPtr+2));
+  printf("*(tmpPtr+3) is %d\n",*(tmpPtr+3));
+
 }
 
 int main(void)
